2971806b0b34d02101dc13b6c6a2043f07d78fac
pishlo <soni.kozaliev@gmail.com>
2025-03-14 11:01:47 +0100

    Assignment for the middleware and the request object

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    console.log( "Time:", Date.now() )
-    next()
-   })
+    const arr_value = 4 * 7;
+
+    console.log("Time:", Date.now());
+    console.log(arr_value);
+
+    res.locals.arr_value = arr_value;
+    next();
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+    res.send(`Arr Value: ${res.locals.arr_value}`);
 });
 
 app.listen(port, () => {
