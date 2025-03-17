d788e4c28245174fa8ec23e3b6716e73296c4a1e
pishlo <soni.kozaliev@gmail.com>
2025-03-17 16:16:51 +0100

    mongo db connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const userRoutes = require("./routes/users");
+const connectDB = require("./utils/db");
+
+connectDB();
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
---
+++ src/utils/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+    console.log("trying")
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ Connected to MongoDB");
+    } catch (err) {
+        console.error("❌ MongoDB connection error:", err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
